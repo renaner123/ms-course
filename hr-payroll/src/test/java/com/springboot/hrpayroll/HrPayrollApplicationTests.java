c0498f05da9e0213437bb6eb42eecb55433122bf
renaner123 <renaner123@gmail.com>
2024-01-09 19:50:35 -0300

    feat: Create hr-payroll project

---
+++ hr-payroll/src/test/java/com/springboot/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
