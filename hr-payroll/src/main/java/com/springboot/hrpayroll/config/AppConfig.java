f892655bf56073e49bd35388a3c67f4ca0f209e4
renaner123 <renaner123@gmail.com>
2024-01-09 21:11:09 -0300

    feat: add resttemplate on Payment

---
+++ hr-payroll/src/main/java/com/springboot/hrpayroll/config/AppConfig.java
@@ -0,0 +1,17 @@
+package com.springboot.hrpayroll.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		
+		//singleton
+		return new RestTemplate();
+	}
+
+}
