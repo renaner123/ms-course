73488a0e554d5bab3ad327df475e42065d26d661
renaner123 <renaner123@gmail.com>
2024-01-10 22:25:36 -0300

    feat: create hr-config-server

---
+++ hr-config-server/src/test/java/com/springboot/hrhrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.hrhrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
