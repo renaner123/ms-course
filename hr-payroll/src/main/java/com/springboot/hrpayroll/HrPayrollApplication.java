7b365a12939325b84a5d424204e431e97f06c060
renaner123 <renaner123@gmail.com>
2024-01-10 21:24:15 -0300

    feat: add hystrix fault tolerance

--- hr-payroll/src/main/java/com/springboot/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/springboot/hrpayroll/HrPayrollApplication.java
@@ -2,10 +2,12 @@ package com.springboot.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
