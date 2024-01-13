ad54ef3a1b5b17b8d5f7ed0d1e376b242fa17a0d
renaner123 <renaner123@gmail.com>
2024-01-13 00:29:34 -0300

    fix: adjustment zuul error management

--- hr-worker/src/main/java/com/springboot/hrworker/repositories/WorkerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.hrworker.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.hrworker.entities.Worker;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long> {
-
-}
